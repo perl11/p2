e9685f933d9e56b5c02da0e13dd080154e7daa0e
Reini Urban <rurban@cpanel.net>
2013-09-26 13:43:59 -0500

    compile for: list expr...

--- core/compile.c
+++ core/compile.c
@@ -714,19 +714,22 @@ void potion_source_asmb(Potion *P, struct PNProto * volatile f, struct PNLoop *l
       } else if (t->part == AST_MSG && PN_S(t,0) == PN_foreach) {
         DBG_c("foreach "); //MSG foreach (itervar list) block
         // list = tuple(decl|var)|table(decl|var)|range(from..to)
-        PN var = potion_tuple_shift(P,0,PN_S(t,1));
-        PN list = PN_S(t,1);
-        // compile block to closure taking the key as arg
-        if (PN_IS_TABLE(list) || PN_IS_TUPLE(list)) {
-          // each for tuple or table
-          DBG_c("each %s %s\n", AS_STR(var), AS_STR(list));
-          potion_send(list, PN_STR("each"), PN_S_(t,2), var);
-        } else if (PN_PART(list) == AST_MSG && PN_S(list,0) == PN_range) {
+        PN k = potion_tuple_shift(P,0,PN_S(t,1));
+        vPN(Source) l = PN_S_(PN_S(t,1),0);
+        // compile block to closure taking the key as arg //XXX
+        if (l->part == AST_LIST) {
+          // each for tuple|table|expr
+          PN_TUPLE_EACH(PN_S(l,0), i, v, {
+            DBG_c("each %s %s {}\n", AS_STR(k), AS_STR(v));
+            //potion_send(v, PN_STR("each"), PN_S_(t,2), k);//XXX
+          });
+        }
+        else if (l->part == AST_MSG && PN_S(l,0) == PN_range) {
           // or from to(to) for range (from..to)
-          PN from = PN_S(list,1);
-          PN to   = PN_S(list,2);
-          DBG_c("range %s (%s..%s)\n", AS_STR(var), AS_STR(from), AS_STR(to));
-          potion_send(from, PN_STR("to"), to, PN_S_(t,2), var);
+          PN from = PN_S(l,1);
+          PN to   = PN_S(l,2);
+          DBG_c("range %s (%s..%s) {}\n", AS_STR(k), AS_STR(from), AS_STR(to));
+          //potion_send(from, PN_STR("to"), to, PN_S_(t,2), k);//XXX
         }
       } else if (t->part == AST_MSG && PN_S(t,0) == PN_for) {
         DBG_c("for nyi"); //MSG for LIST(s1;s2;s3) block
