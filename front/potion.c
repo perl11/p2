perl11/potion

fixes for clang3.3 --analyze static analyzer
most caused by possible null dereferences.

NOT MERGABLE YET. broke even some bytecode tests:
  classes/creature.pn classes/def.pn classes/sub.pn flow/except.pn
  tutorial/07_subclass.pn

front/potion.c
@@ -93,13 +93,15 @@ static PN potion_cmd_exec(Potion *P, PN buf, char *filename, exec_mode_t exec) {
   } else if (exec == EXEC_JIT) {
 #ifdef POTION_JIT_TARGET
     PN val;
-    PN cl = potion_closure_new(P, (PN_F)potion_jit_proto(P, code), PN_NIL, 1);
-    PN_CLOSURE(cl)->data[0] = code;
-    val = PN_PROTO(code)->jit(P, cl, P->lobby);
-    dbg_v("\n-- jit returned %p (fixed=%ld, actual=%ld, reserved=%ld) --\n", PN_PROTO(code)->jit,
-	  PN_INT(potion_gc_fixed(P, 0, 0)), PN_INT(potion_gc_actual(P, 0, 0)),
-	  PN_INT(potion_gc_reserved(P, 0, 0)));
-    dbgP_vi(val);
+    if (code) {
+      PN cl = potion_closure_new(P, (PN_F)potion_jit_proto(P, code), PN_NIL, 1);
+      PN_CLOSURE(cl)->data[0] = code;
+      val = PN_PROTO(code)->jit(P, cl, P->lobby);
+      dbg_v("\n-- jit returned %p (fixed=%ld, actual=%ld, reserved=%ld) --\n", PN_PROTO(code)->jit,
+            PN_INT(potion_gc_fixed(P, 0, 0)), PN_INT(potion_gc_actual(P, 0, 0)),
+            PN_INT(potion_gc_reserved(P, 0, 0)));
+      dbgP_vi(val);
+    }
 #else
     fprintf(stderr, "** potion built without JIT support\n");
 #endif
@@ -158,14 +160,16 @@ static void potion_cmd_compile(Potion *P, char *filename, exec_mode_t exec) {
     dbgP_vi(code);
   } else if (exec == EXEC_JIT) {
 #ifdef POTION_JIT_TARGET
-    PN val;
-    PN cl = potion_closure_new(P, (PN_F)potion_jit_proto(P, code), PN_NIL, 1);
-    PN_CLOSURE(cl)->data[0] = code;
-    val = PN_PROTO(code)->jit(P, cl, P->lobby);
-    dbg_v("\n-- jit returned %p (fixed=%ld, actual=%ld, reserved=%ld) --\n", PN_PROTO(code)->jit,
-	  PN_INT(potion_gc_fixed(P, 0, 0)), PN_INT(potion_gc_actual(P, 0, 0)),
-	  PN_INT(potion_gc_reserved(P, 0, 0)));
-    dbgP_vi(val);
+    if (code) {
+      PN val;
+      PN cl = potion_closure_new(P, (PN_F)potion_jit_proto(P, code), PN_NIL, 1);
+      PN_CLOSURE(cl)->data[0] = code;
+      val = PN_PROTO(code)->jit(P, cl, P->lobby);
+      dbg_v("\n-- jit returned %p (fixed=%ld, actual=%ld, reserved=%ld) --\n", PN_PROTO(code)->jit,
+            PN_INT(potion_gc_fixed(P, 0, 0)), PN_INT(potion_gc_actual(P, 0, 0)),
+            PN_INT(potion_gc_reserved(P, 0, 0)));
+      dbgP_vi(val);
+    }
 #else
     fprintf(stderr, "** potion built without JIT support\n");
 #endif
@@ -209,7 +213,7 @@ static void potion_cmd_compile(Potion *P, char *filename, exec_mode_t exec) {
 	potion_fatal("--compile-native not yet implemented\n");
       }
 
-      if (fwrite(PN_STR_PTR(code), 1, PN_STR_LEN(code), pnb) == PN_STR_LEN(code)) {
+      if (code && (fwrite(PN_STR_PTR(code), 1, PN_STR_LEN(code), pnb) == PN_STR_LEN(code))) {
         printf("** compiled code saved to %s\n", pnbpath);
         fclose(pnb);
 

perl11/p2

front/potion.c
@@ -137,14 +137,17 @@ static PN potion_cmd_exec(Potion *P, PN buf, char *filename, char *compile, char
   } else if (exec == EXEC_JIT) {
 #ifdef POTION_JIT_TARGET
     PN val;
-    PN cl = potion_closure_new(P, (PN_F)potion_jit_proto(P, code), PN_NIL, 1);
-    PN_CLOSURE(cl)->data[0] = code;
-    val = PN_PROTO(code)->jit(P, cl, P->lobby);
-    DBG_v("\n-- jit returned %p (fixed=%ld, actual=%ld, reserved=%ld, time=%0.6gms %dx/%dm/%di) --\n", PN_PROTO(code)->jit,
+    if (code) {
+      PN cl = potion_closure_new(P, (PN_F)potion_jit_proto(P, code), PN_NIL, 1);
+      PN_CLOSURE(cl)->data[0] = code;
+      val = PN_PROTO(code)->jit(P, cl, P->lobby);
+      DBG_v("\n-- jit returned %p (fixed=%ld, actual=%ld, reserved=%ld, time=%0.6gms %dx/%dm/%di) --\n",
+          PN_PROTO(code)->jit,
 	  PN_INT(potion_gc_fixed(P, 0, 0)), PN_INT(potion_gc_actual(P, 0, 0)),
 	  PN_INT(potion_gc_reserved(P, 0, 0)), P->mem->time * 1000, P->mem->pass,
 	  P->mem->majors, P->mem->minors);
-    DBG_Pvi(val);
+      DBG_Pvi(val);
+    }
 #else
     fprintf(stderr, "** potion built without JIT support\n");
 #endif
@@ -384,7 +387,7 @@ int main(int argc, char *argv[]) {
     }
   }
   P->flags = P->flags + exec;
-  
+
   if (buf || i < argc) {
     PN args = PN_TUP0();
     if (buf == PN_NIL) { fn = argv[i++]; PN_PUSH(args, PN_STR(fn)); }